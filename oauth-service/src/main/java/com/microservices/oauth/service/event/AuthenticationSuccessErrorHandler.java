d3466a1d26c2488ba49c91662422a741d1742815
jaime1991 <jaime1991@misena.edu.co>
2020-02-08 18:03:56 -0500

    Zipkin and slugth

--- oauth-service/src/main/java/com/microservices/oauth/service/event/AuthenticationSuccessErrorHandler.java
+++ oauth-service/src/main/java/com/microservices/oauth/service/event/AuthenticationSuccessErrorHandler.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import com.microservices.oauth.service.models.entity.User;
 import com.microservices.oauth.service.services.IUserService;
 
+import brave.Tracer;
 import feign.FeignException;
 
 @Component
@@ -19,6 +20,8 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 
 	@Autowired
 	private IUserService userService;
+	@Autowired
+	private Tracer tracer;
 	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
 	
 	@Override
@@ -35,8 +38,11 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 
 	@Override
 	public void publishAuthenticationFailure(AuthenticationException exception, Authentication authentication) {
-		log.error("Error in login: " + exception.getMessage());
+		String error = "Error in login: " + exception.getMessage();
+		log.error(error);
 		try {
+			StringBuilder errors = new StringBuilder();
+			errors.append(error);
 			User user = userService.findByUsername(authentication.getName());
 			if(user.getAttempts() == null) {
 				user.setAttempts(0);
@@ -45,12 +51,17 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 			log.info(String.format("current attempts: %d", user.getAttempts()));
 			user.setAttempts(user.getAttempts() + 1);
 			
+			errors.append(" - current attempts:" + user.getAttempts());
+			
 			if(user.getAttempts() >= 3) {
-				log.error(String.format("Maximun attempts exceded by user %s", user.getName()));
+				error = String.format(" - Maximun attempts exceded by user %s", user.getName());
+				errors.append(error);
+				log.error(error);
 				user.setEnabled(false);
 			}
 			
 			userService.update(user, user.getId());
+			tracer.currentSpan().tag("error.message", errors.toString());
 		}catch(FeignException e) {
 			log.error(String.format("The username %s not exist in the system", authentication.getName()));
 		}
--- oauth-service/src/main/java/com/microservices/oauth/service/services/UserService.java
+++ oauth-service/src/main/java/com/microservices/oauth/service/services/UserService.java
@@ -16,33 +16,38 @@ import org.springframework.stereotype.Service;
 import com.microservices.oauth.service.clients.UserFeignClient;
 import com.microservices.oauth.service.models.entity.User;
 
+import brave.Tracer;
+import feign.FeignException;
+
 @Service
-public class UserService implements IUserService, UserDetailsService{
+public class UserService implements IUserService, UserDetailsService {
 
 	@Autowired
 	private UserFeignClient client;
+	@Autowired
+	private Tracer tracer;
 	private Logger log = LoggerFactory.getLogger(UserService.class);
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = client.findByUsername(username);
+		try {
+			User user = client.findByUsername(username);
 
-		if (user == null) {
-			log.error("Error in login. Username not found");
-			throw new UsernameNotFoundException("Error in login. Username not found");
+			List<GrantedAuthority> authorities = user.getRoles().stream()
+					.map(role -> new SimpleGrantedAuthority(role.getName()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
+
+			log.info("login success. User: " + user.getName());
+
+			return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+					user.getEnabled(), true, true, true, authorities);
+		} catch (FeignException e) {
+			String error = "Error in login. Username not found " + username;
+			log.error(error);
+			tracer.currentSpan().tag("error.message", error + ": " + e.getMessage());
+			throw new UsernameNotFoundException(error);
 		}
 
-		List<GrantedAuthority> authorities = user.getRoles()
-				.stream()
-				.map(role -> new SimpleGrantedAuthority(role.getName()))
-				.peek(authority -> log.info("Role: " + authority.getAuthority()))
-				.collect(Collectors.toList());
-		
-		log.info("Username: " + username);
-		
-		return new org.springframework.security.core.userdetails.User(
-				user.getUsername(), user.getPassword(),
-				user.getEnabled(), true, true, true, authorities);
 	}
 
 	@Override
